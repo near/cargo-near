70595a3a4e0dc50e4ce7817c04509f8caa9be2ee
Miraculous Owonubi <omiraculous@gmail.com>
2022-08-29 13:54:27 +0400

    use shared target folder for faster CI (#39)

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -21,6 +21,8 @@ macro_rules! generate_abi {
         let lib_rs_path = src_dir.join("lib.rs");
         fs::write(lib_rs_path, lib_rs)?;
 
+        std::env::set_var("CARGO_TARGET_DIR", workspace_dir.join("target"));
+
         cargo_near::exec(cargo_near::NearCommand::Abi(cargo_near::AbiCommand {
             manifest_path: Some(cargo_path),
             doc: false,
@@ -28,7 +30,7 @@ macro_rules! generate_abi {
         }))?;
 
         let abi_root: near_abi::AbiRoot =
-            serde_json::from_slice(&fs::read(workspace_dir.join(function_name!()).join("target").join("near").join("abi.json"))?)?;
+        serde_json::from_slice(&fs::read(workspace_dir.join("target").join("near").join(format!("{}_abi.json", function_name!())))?)?;
         abi_root
     }};
     (with Cargo $cargo_path:expr; $($code:tt)*) => {
