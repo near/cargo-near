bade9498eb581bf7518961746f7a54b9d1fef246
Miraculous Owonubi <omiraculous@gmail.com>
2022-08-31 18:37:25 +0400

    add check for supported near-sdk version (#49)
    Co-authored-by: Daniyar Itegulov <ditegulov@gmail.com>

--- cargo-near/src/abi.rs
+++ cargo-near/src/abi.rs
@@ -27,24 +27,48 @@ pub(crate) fn generate_abi(
     crate_metadata: &CrateMetadata,
     generate_docs: bool,
 ) -> anyhow::Result<AbiRoot> {
-    fs::create_dir_all(&crate_metadata.target_directory)?;
+    let root_node = crate_metadata
+        .raw_metadata
+        .resolve
+        .as_ref()
+        .and_then(|dep_graph| {
+            dep_graph
+            .nodes
+            .iter()
+            .find(|node| node.id == crate_metadata.root_package.id)
+        })
+        .ok_or_else(|| anyhow::anyhow!("unable to appropriately resolve the dependency graph, perhaps your `Cargo.toml` file is malformed"))?;
 
-    let original_cargo_toml: toml::value::Table =
-        toml::from_slice(&fs::read(&crate_metadata.manifest_path.path)?)?;
+    let near_sdk_dep = root_node
+        .deps
+        .iter()
+        .find(|dep| dep.name == "near_sdk")
+        .and_then(|near_sdk| {
+            crate_metadata
+                .raw_metadata
+                .packages
+                .iter()
+                .find(|pkg| pkg.id == near_sdk.pkg)
+        })
+        .ok_or_else(|| anyhow::anyhow!("`near-sdk` dependency not found"))?;
 
-    if !original_cargo_toml
-        .get("dependencies")
-        .ok_or_else(|| anyhow::anyhow!("[dependencies] section not found"))?
-        .get("near-sdk")
-        .ok_or_else(|| anyhow::anyhow!("near-sdk dependency not found"))?
-        .as_table()
-        .ok_or_else(|| anyhow::anyhow!("near-sdk dependency should be a table"))?
-        .get("features")
-        .and_then(|features| features.as_array())
-        .map(|features| features.contains(&toml::Value::String("abi".to_string())))
-        .unwrap_or(false)
+    for required_feature in ["__abi-generate", "__abi-embed"] {
+        if !near_sdk_dep.features.contains_key(required_feature) {
+            anyhow::bail!("unsupported `near-sdk` version. expected 4.1.* or higher");
+        }
+    }
+
+    if !crate_metadata
+        .root_package
+        .dependencies
+        .iter()
+        .find(|dep| dep.name == "near-sdk")
+        .ok_or_else(|| anyhow::anyhow!("`near-sdk` dependency not found"))?
+        .features
+        .iter()
+        .any(|feature| feature == "abi")
     {
-        anyhow::bail!("Unable to generate ABI: NEAR SDK \"abi\" feature is not enabled")
+        anyhow::bail!("`near-sdk` dependency must have the `abi` feature enabled")
     }
 
     let dylib_artifact = util::compile_project(
@@ -87,6 +111,8 @@ pub(crate) fn write_to_file(
             *zstd::compression_level_range().end(),
         )?,
     };
+
+    fs::create_dir_all(&crate_metadata.target_directory)?;
 
     let out_path_abi = crate_metadata.target_directory.join(format!(
         "{}_abi.{}",
