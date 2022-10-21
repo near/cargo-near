1ce7787ca77c58e052412b78dbf82f664cee1744
Miraculous Owonubi <omiraculous@gmail.com>
2022-10-21 08:46:07 +0400

    update sdk ver

--- integration-tests/tests/cargo/mod.rs
+++ integration-tests/tests/cargo/mod.rs
@@ -20,7 +20,7 @@ fn clone_git_repo(version: &str) -> anyhow::Result<TempDir> {
 #[test]
 #[named]
 fn test_dependency_local_path() -> anyhow::Result<()> {
-    let near_sdk_dir = clone_git_repo("9a7d7ebbed53911d764f49419acd140c1162928f")?;
+    let near_sdk_dir = clone_git_repo("cb41e379f70e1bf0eb5df7b9c06ec76cc1734d80")?;
     let near_sdk_dep_path = near_sdk_dir.path().join("near-sdk");
 
     // near-sdk = { path = "::path::", features = ["abi"] }
@@ -42,7 +42,7 @@ fn test_dependency_local_path() -> anyhow::Result<()> {
 #[test]
 #[named]
 fn test_dependency_local_path_with_version() -> anyhow::Result<()> {
-    let near_sdk_dir = clone_git_repo("9a7d7ebbed53911d764f49419acd140c1162928f")?;
+    let near_sdk_dir = clone_git_repo("cb41e379f70e1bf0eb5df7b9c06ec76cc1734d80")?;
     let near_sdk_dep_path = near_sdk_dir.path().join("near-sdk");
 
     // near-sdk = { path = "::path::", version = "4.1.0-pre.3", features = ["abi"] }
@@ -178,7 +178,7 @@ fn test_dependency_patch() -> anyhow::Result<()> {
     // near-sdk = "4.0.0"
     //
     // [patch.crates-io]
-    // near-sdk = { git = "https://github.com/near/near-sdk-rs.git", rev = "9a7d7ebbed53911d764f49419acd140c1162928f" }
+    // near-sdk = { git = "https://github.com/near/near-sdk-rs.git", rev = "cb41e379f70e1bf0eb5df7b9c06ec76cc1734d80" }
     let abi_root = generate_abi_fn_with! {
         Cargo: "/templates/sdk-dependency/_Cargo_patch.toml";
         Code:
