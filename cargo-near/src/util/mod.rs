208cef385d83493e3abfcbdcc3a6d56242294c65
Vlad Frolov <frolvlad@gmail.com>
2023-10-11 23:47:40 +0200

    Apply suggestions from code review

--- cargo-near/src/util/mod.rs
+++ cargo-near/src/util/mod.rs
@@ -55,9 +55,7 @@ where
     cmd.envs(env);
 
     if let Some(path) = working_dir {
-        let path = path.as_ref();
-        // remove UNC prefix to be able to compile on Windows
-        let path = force_canonicalize_dir(path)?;
+        let path = force_canonicalize_dir(path.as_ref())?;
         log::debug!("Setting cargo working dir to '{}'", path);
         cmd.current_dir(path);
     }
@@ -244,13 +242,13 @@ pub(crate) fn compile_project(
 pub(crate) fn force_canonicalize_dir(dir: &Utf8Path) -> color_eyre::eyre::Result<Utf8PathBuf> {
     fs::create_dir_all(dir).wrap_err_with(|| format!("failed to create directory `{}`", dir))?;
     // use canonicalize from `dunce` create instead of default one from std because it's compatible with Windows UNC paths
-    // and don't breake cargo compilation on Windows
+    // and don't break cargo compilation on Windows
     // https://github.com/rust-lang/rust/issues/42869
     Utf8PathBuf::from_path_buf(
         dunce::canonicalize(dir)
             .wrap_err_with(|| format!("failed to canonicalize path: {} ", dir))?,
     )
-    .map_err(|err| color_eyre::eyre::eyre!(" failed to convert path {}", err.to_string_lossy()))
+    .map_err(|err| color_eyre::eyre::eyre!("failed to convert path {}", err.to_string_lossy()))
 }
 
 /// Copy a file to a destination.
