576cd2513d49eacb2a5eb55b260196792710d552
FroVolod <frol_off@meta.ua>
2023-10-15 09:34:04 +0300

    refactored

--- cargo-near/src/commands/build_command/mod.rs
+++ cargo-near/src/commands/build_command/mod.rs
@@ -1,6 +1,6 @@
 pub mod build;
 
-#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[derive(Debug, Default, Clone, interactive_clap::InteractiveClap)]
 #[interactive_clap(context = near_cli_rs::GlobalContext)]
 #[interactive_clap(skip_default_from_cli)]
 pub struct BuildCommand {
@@ -36,7 +36,7 @@ impl interactive_clap::FromCli for BuildCommand {
     type FromCliError = color_eyre::eyre::Error;
     fn from_cli(
         optional_clap_variant: Option<<Self as interactive_clap::ToCli>::CliVariant>,
-        context: Self::FromCliContext,
+        _context: Self::FromCliContext,
     ) -> interactive_clap::ResultFromCli<
         <Self as interactive_clap::ToCli>::CliVariant,
         Self::FromCliError,
@@ -44,41 +44,15 @@ impl interactive_clap::FromCli for BuildCommand {
     where
         Self: Sized + interactive_clap::ToCli,
     {
-        let mut clap_variant = optional_clap_variant.unwrap_or_default();
-        let release = clap_variant.release;
-        let embed_abi = clap_variant.embed_abi;
-        let doc = clap_variant.doc;
-        let no_abi = clap_variant.no_abi;
-        if clap_variant.out_dir.is_none() {
-            clap_variant.out_dir = match Self::input_out_dir(&context) {
-                Ok(optional_out_dir) => optional_out_dir,
-                Err(err) => return interactive_clap::ResultFromCli::Err(Some(clap_variant), err),
-            };
-        };
-        let out_dir = clap_variant.out_dir.clone();
-        if clap_variant.manifest_path.is_none() {
-            clap_variant.manifest_path = match Self::input_manifest_path(&context) {
-                Ok(optional_manifest_path) => optional_manifest_path,
-                Err(err) => return interactive_clap::ResultFromCli::Err(Some(clap_variant), err),
-            };
-        };
-        let manifest_path = clap_variant.manifest_path.clone();
-        if clap_variant.color.is_none() {
-            clap_variant.color = match Self::input_color(&context) {
-                Ok(optional_color) => optional_color,
-                Err(err) => return interactive_clap::ResultFromCli::Err(Some(clap_variant), err),
-            };
-        };
-        let color = clap_variant.color.clone();
-
+        let clap_variant = optional_clap_variant.unwrap_or_default();
         let args = Self {
-            release,
-            embed_abi,
-            doc,
-            no_abi,
-            out_dir,
-            manifest_path,
-            color,
+            release: clap_variant.release,
+            embed_abi: clap_variant.embed_abi,
+            doc: clap_variant.doc,
+            no_abi: clap_variant.no_abi,
+            out_dir: clap_variant.out_dir.clone(),
+            manifest_path: clap_variant.manifest_path.clone(),
+            color: clap_variant.color.clone(),
         };
         if let Err(err) = self::build::run(args).map(|_| ()) {
             interactive_clap::ResultFromCli::Err(Some(clap_variant), color_eyre::eyre::eyre!(err))
@@ -89,18 +63,21 @@ impl interactive_clap::FromCli for BuildCommand {
 }
 
 impl BuildCommand {
+    #[allow(dead_code)]
     fn input_color(
         _context: &near_cli_rs::GlobalContext,
     ) -> color_eyre::eyre::Result<Option<crate::common::ColorPreference>> {
         Ok(None)
     }
 
+    #[allow(dead_code)]
     fn input_out_dir(
         _context: &near_cli_rs::GlobalContext,
     ) -> color_eyre::eyre::Result<Option<crate::types::utf8_path_buf::Utf8PathBufInner>> {
         Ok(None)
     }
 
+    #[allow(dead_code)]
     fn input_manifest_path(
         _context: &near_cli_rs::GlobalContext,
     ) -> color_eyre::eyre::Result<Option<crate::types::utf8_path_buf::Utf8PathBufInner>> {
