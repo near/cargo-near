a39f81e615d48a94f9794eb141b888b01aaaee82
FroVolod <frol_off@meta.ua>
2023-11-14 20:40:59 +0200

    start

---
+++ cargo-near/src/commands/new/mod.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(input_context = near_cli_rs::GlobalContext)]
+#[interactive_clap(output_context = NewContext)]
+pub struct New {
+    /// Enter a new project name to create a contract:
+    pub project_dir: near_cli_rs::types::path_buf::PathBuf,
+}
+
+#[derive(Debug, Clone)]
+pub struct NewContext;
+
+impl NewContext {
+    pub fn from_previous_context(
+        _previous_context: near_cli_rs::GlobalContext,
+        scope: &<New as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        let project_dir = scope.project_dir.clone();
+        std::process::Command::new("cargo")
+            .arg("new")
+            .arg(&project_dir)
+            .arg("--lib")
+            .output()
+            .expect("failed to execute process");
+        Ok(Self)
+    }
+}
