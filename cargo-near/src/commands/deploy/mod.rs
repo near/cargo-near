43b47b879d819e27941b82cc4cfef782ad26478a
dj8yf0μl <feepdake27@gmail.com>
2024-04-09 18:52:44 +0300

    todo: comment section with git pushed check in `cargo near deploy` flow

--- cargo-near/src/commands/deploy/mod.rs
+++ cargo-near/src/commands/deploy/mod.rs
@@ -38,16 +38,21 @@ impl ContractContext {
                 })?
             };
 
-        eprintln!(
-            "\nThe URL of the remote repository:\n{}\n",
-            remote_repo_url(&contract_path)?
-        );
 
         let file_path = if !scope.build_command_args.no_docker {
             build_command::docker_run(scope.build_command_args.clone())?
         } else {
             build_command::build::run(scope.build_command_args.clone())?.path
         };
+        // TODO: rework flow with checkout after contract built before deploy 
+        // NOTE:  `git clone https://github.com/dj8yfo/sample_no_workspace/tree/73f5eb98c257fd9115675f3894ddfd37a5915e7b `
+        // and `git clone https://github.com/dj8yfo/sample_no_workspace.git/commit/73f5eb98c257fd9115675f3894ddfd37a5915e7`
+        // are both an error
+        // TODO: clone to tmp folder and checkout specific revision must be separate steps
+        // eprintln!(
+        //     "\nThe URL of the remote repository:\n{}\n",
+        //     remote_repo_url(&contract_path)?
+        // );
 
         Ok(Self(
             near_cli_rs::commands::contract::deploy::ContractFileContext {
