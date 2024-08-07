7805c8e3b0696eb5306fabee80299cc77c7d38ea
garikbesson <garikbesson@gmail.com>
2024-08-07 13:05:08 +0200

    tracking pkg (cargo-near) version instead of cli (near-cli-rs) version

--- cargo-near/src/util/tracking.rs
+++ cargo-near/src/util/tracking.rs
@@ -1,5 +1,5 @@
 use reqwest::Client;
-use std::{env, process::Command, str};
+use std::{env, str};
 use tracing::debug;
 
 const SEND_TRACKING_REQUEST_ERROR: &str = "Can't send tracking usage event";
@@ -7,7 +7,7 @@ const SEND_TRACKING_REQUEST_ERROR: &str = "Can't send tracking usage event";
 #[derive(Debug, serde::Serialize)]
 struct MixpanelProperties {
     token: String,
-    cli_version: String,
+    pkg_version: String,
     os: String,
 }
 
@@ -17,41 +17,10 @@ struct TrackingData {
     properties: MixpanelProperties,
 }
 
-fn trim_newline(s: String) -> String {
-    let mut result = s;
-
-    if result.ends_with('\n') {
-        result.pop();
-        if result.ends_with('\r') {
-            result.pop();
-        }
-    }
-
-    result
-}
-
 pub(crate) fn track_usage() {
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", "near --version"])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg("near --version")
-            .output()
-            .expect("failed to execute process")
-    };
-
-    let cli_version: String = match str::from_utf8(&output.stdout) {
-        Ok(v) => v.split(" ").collect::<Vec<_>>()[1].to_string(),
-        Err(_) => "unknown".to_string(),
-    };
-
     let properties = MixpanelProperties {
         token: "24177ef1ec09ffea5cb6f68909c66a61".to_string(),
-        cli_version: trim_newline(cli_version),
+        pkg_version: env!("CARGO_PKG_VERSION").to_string(),
         os: env::consts::OS.to_string(),
     };
     let tracking_data = TrackingData {
