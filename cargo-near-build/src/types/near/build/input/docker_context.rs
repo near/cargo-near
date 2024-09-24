e6a3be9e940dc8fb7aab07a7607b7973d4be11ff
dj8yf0μl <noreply@nowhere.org>
2024-09-24 14:22:21 +0300

    review: fix nit https://github.com/near/cargo-near/pull/226#discussion_r1770905350

--- cargo-near-build/src/types/near/build/input/docker_context.rs
+++ cargo-near-build/src/types/near/build/input/docker_context.rs
@@ -21,46 +21,55 @@ impl super::Opts {
         manifest_command: Option<Vec<String>>,
         passed_env: Option<Vec<String>>,
     ) -> eyre::Result<Vec<String>> {
-        manifest_command.map_or_else(
-            || {
+        if let Some(manifest_command) = manifest_command {
+            self.append_env_suffix(manifest_command, passed_env)
+        } else {
+            println!(
+                "{}",
+                "configuring `container_build_command` from cli args, passed to current command"
+                    .cyan()
+            );
+            println!();
+            Ok(self.passthrough_some_opts_into_docker_cmd())
+        }
+    }
+
+    fn append_env_suffix(
+        &self,
+        mut manifest_command: Vec<String>,
+        passed_env: Option<Vec<String>>,
+    ) -> eyre::Result<Vec<String>> {
+        self.check_flag_conflicts_with_manifest_command()?;
+        let suffix_env = passed_env
+            .into_iter()
+            .flatten()
+            .filter(|env_key| std::env::var(env_key).is_ok())
+            .flat_map(|env_key| {
                 println!(
-                    "{}",
-                    "configuring `container_build_command` from cli args, passed to current command".cyan()
+                    "{}{}{}",
+                    "detected environment build parameter, which has been set: `".cyan(),
+                    env_key.yellow(),
+                    "`".cyan(),
                 );
-                println!();
-                Ok(self.passthrough_some_opts_into_docker_cmd())
-            },
-            |mut manifest_command| {
-                self.check_flag_conflicts_with_manifest_command()?;
-                let suffix_env = passed_env.into_iter().flatten().filter(|env_key| {
-                    std::env::var(env_key).is_ok()
-                }).flat_map(|env_key| {
-                    println!(
-                        "{}{}{}", 
-                        "detected environment build parameter, which has been set: `".cyan(), 
-                        env_key.yellow(),
-                        "`".cyan(),
-                    );
-                    let value = std::env::var(&env_key).unwrap();
-                    let pair = [env_key, value].join("=");
-                    ["--env".to_string(), pair]
-                }).collect::<Vec<_>>();
+                let value = std::env::var(&env_key).unwrap();
+                let pair = [env_key, value].join("=");
+                ["--env".to_string(), pair]
+            })
+            .collect::<Vec<_>>();
 
-                if !suffix_env.is_empty() {
-                    println!("{}{}{}",
-                        "(listed in `".cyan(), 
-                        "passed_env".yellow(), 
-                        "` from `[package.metadata.near.reproducible_build]` in Cargo.toml)".cyan(),
-                    );
-                    println!();
-                }
+        if !suffix_env.is_empty() {
+            println!(
+                "{}{}{}",
+                "(listed in `".cyan(),
+                "passed_env".yellow(),
+                "` from `[package.metadata.near.reproducible_build]` in Cargo.toml)".cyan(),
+            );
+            println!();
+        }
 
+        manifest_command.extend(suffix_env);
 
-                manifest_command.extend(suffix_env);
-
-                Ok(manifest_command)
-            }
-        )
+        Ok(manifest_command)
     }
 
     fn passthrough_some_opts_into_docker_cmd(&self) -> Vec<String> {
