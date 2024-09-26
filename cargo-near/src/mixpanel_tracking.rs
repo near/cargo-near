0cdc0666146d92d54aa01e37b10f79b1440b16e2
FroVolod <frol_off@meta.ua>
2024-09-26 15:29:23 +0300

    reverted a fix for track_usage()

--- cargo-near/src/mixpanel_tracking.rs
+++ cargo-near/src/mixpanel_tracking.rs
@@ -2,7 +2,7 @@ use base64::{engine::general_purpose, Engine as _};
 use reqwest::{header::HeaderMap, Client};
 use serde::Serialize;
 use std::{env, str};
-use tracing_indicatif::span_ext::IndicatifSpanExt;
+use tracing::debug;
 
 const SEND_TRACKING_REQUEST_ERROR: &str = "Can't send tracking usage event";
 
@@ -19,14 +19,7 @@ struct TrackingData {
     properties: MixpanelProperties,
 }
 
-#[tracing::instrument(
-    target = "tracing_instrument",
-    name = "Sending a tracking request using Mixpanel via url"
-)]
 pub(crate) fn track_usage() {
-    tracing::Span::current().pb_set_message("https://api.mixpanel.com/track ...");
-    tracing::info!(target: "near_teach_me", "https://api.mixpanel.com/track ...");
-
     let properties = MixpanelProperties {
         token: "24177ef1ec09ffea5cb6f68909c66a61".to_string(),
         pkg_version: env!("CARGO_PKG_VERSION").to_string(),
@@ -41,26 +34,6 @@ pub(crate) fn track_usage() {
 
     let client = Client::new();
 
-    let request_payload = serde_json::json!({
-       "base64_encoded_tracking_data": base64_encoded_data
-    });
-    tracing::info!(
-        target: "near_teach_me",
-        parent: &tracing::Span::none(),
-        "I am making HTTP call to broadcast the tracking data, learn more https://mixpanel.com/"
-    );
-    tracing::info!(
-        target: "near_teach_me",
-        parent: &tracing::Span::none(),
-        "HTTP GET https://api.mixpanel.com/track",
-    );
-    tracing::info!(
-        target: "near_teach_me",
-        parent: &tracing::Span::none(),
-        "JSON Body:\n{}",
-        near_cli_rs::common::indent_payload(&format!("{:#}", request_payload))
-    );
-
     let mut headers = HeaderMap::new();
     headers.insert("accept", "text/plain".parse().unwrap());
     headers.insert("content-type", "application/json".parse().unwrap());
@@ -70,27 +43,13 @@ pub(crate) fn track_usage() {
         .block_on(
             client
                 .get("https://api.mixpanel.com/track")
-                .query(&[("data", &base64_encoded_data)])
-                .headers(headers.clone())
+                .query(&[("data", base64_encoded_data)])
+                .headers(headers)
                 .send(),
         )
-        .inspect(|response| {
-            tracing::info!(
-                target: "near_teach_me",
-                parent: &tracing::Span::none(),
-                "JSON Response:\n{}",
-                near_cli_rs::common::indent_payload(&format!("{:#?}", response))
-            );
-        })
         .is_err()
     {
-        tracing::info!(
-            target: "near_teach_me",
-            parent: &tracing::Span::none(),
-            "JSON RPC Response:\n{}",
-            near_cli_rs::common::indent_payload(SEND_TRACKING_REQUEST_ERROR)
-        );
-        tracing::debug!(SEND_TRACKING_REQUEST_ERROR)
+        debug!(SEND_TRACKING_REQUEST_ERROR)
     }
 }
 
