f7ee6e22f9cf900c9fee05befbcedfe8299f489b
dj8yf0μl <noreply@nowhere.org>
2025-03-24 23:02:10 +0200

    perform a computation of relative path from [crate::env_keys::nep330::CONTRACT_PATH] in `container_paths::Paths`

--- cargo-near-build/src/types/near/docker_build/subprocess/container_paths.rs
+++ cargo-near-build/src/types/near/docker_build/subprocess/container_paths.rs
@@ -1,5 +1,8 @@
-use crate::types::near::docker_build::cloned_repo;
+use std::str::FromStr;
+
 use eyre::ContextCompat;
+
+use super::env_vars::nep330_build_info::BuildInfoMixed;
 
 pub struct Paths {
     pub host_volume_arg: String,
@@ -8,35 +11,18 @@ pub struct Paths {
 
 const NEP330_REPO_MOUNT: &str = "/home/near/code";
 
-/// TODO: #E6 remove this redundant method
-#[allow(unused)]
-fn nep330_contract_path_in_other_spot(
-    cloned_repo: &cloned_repo::ClonedRepo,
-) -> eyre::Result<String> {
-    let contract_path = cloned_repo
-        .initial_crate_in_repo
-        .unix_relative_path()?
-        .to_str()
-        .wrap_err("non UTF-8 unix path computed as contract path")?
-        .to_string();
-
-    Ok(contract_path)
-}
-
-/// TODO: #E5 pass in [crate::types::near::docker_build::subprocess::env_vars::nep330_build_info::BuildInfoMixed::contract_path]
-/// TODO: #E6 perform a computation of relative path from [crate::env_keys::nep330::CONTRACT_PATH]
 impl Paths {
     pub fn compute(
-        cloned_repo: &cloned_repo::ClonedRepo,
+        build_info_mixed: &BuildInfoMixed,
         contract_source_workdir: camino::Utf8PathBuf,
     ) -> eyre::Result<Self> {
         let mounted_repo = NEP330_REPO_MOUNT.to_string();
         let host_volume_arg = format!("{}:{}", contract_source_workdir.as_str(), &mounted_repo);
         let crate_path = {
             let mut repo_path = unix_path::Path::new(NEP330_REPO_MOUNT).to_path_buf();
-            #[allow(unused_doc_comments)]
-            /// TODO #E6 relative_path = unix_path::PathBuf::from_str("path/babe")?;
-            repo_path.push(cloned_repo.initial_crate_in_repo.unix_relative_path()?);
+            let relative_crate_path =
+                unix_path::PathBuf::from_str(&build_info_mixed.contract_path)?;
+            repo_path.push(relative_crate_path);
 
             repo_path
                 .to_str()
