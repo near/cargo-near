f854abc6d3df6c22072f71a0b2a6b04367c7206b
dj8yf0μl <noreply@nowhere.org>
2025-03-24 23:02:10 +0200

    perform a computation of relative path from [crate::env_keys::nep330::CONTRACT_PATH] in `container_paths::Paths`

--- cargo-near-build/src/near/docker_build/subprocess_step.rs
+++ cargo-near-build/src/near/docker_build/subprocess_step.rs
@@ -11,20 +11,16 @@ use nix::unistd::{getgid, getuid};
 
 use crate::env_keys;
 use crate::pretty_print;
-use crate::types::near::docker_build::cloned_repo;
 use crate::types::near::docker_build::subprocess::container_paths;
 use crate::types::near::docker_build::subprocess::env_vars;
 use crate::types::near::docker_build::subprocess::env_vars::nep330_build_info::BuildInfoMixed;
 
 /// TODO #F: set input params to be [near_verify_rs::types::nep330::ContractSourceMetadata] and `contract_sources_workdir`  of [std::path::PathBuf]
 /// TODO: #F1: add [Vec<String>] `additional_docker_args` parameter
-// TODO #E:  the `contract_source_workdir` is defined as `cloned_repo.tmp_repo_dir.path()`
-/// TODO #E7: get rid of [cloned_repo::ClonedRepo] argument
 /// TODO #H2: add validation of [BuildInfoMixed::build_environment] with `images_whitelist` [Vec<String>] argument
 /// TODO #H1: check [BuildInfoMixed::build_environment] for regex match
 pub fn run(
     build_info_mixed: BuildInfoMixed,
-    cloned_repo: &cloned_repo::ClonedRepo,
     contract_source_workdir: camino::Utf8PathBuf,
 ) -> eyre::Result<(ExitStatus, Command)> {
     let mut docker_cmd: Command = {
@@ -50,7 +46,7 @@ pub fn run(
             format!("cargo-near-{}-{}", timestamp, pid)
         };
         let container_paths =
-            container_paths::Paths::compute(cloned_repo, contract_source_workdir)?;
+            container_paths::Paths::compute(&build_info_mixed, contract_source_workdir)?;
 
         let docker_env_args = {
             let env = env_vars::EnvVars::new(build_info_mixed.clone())?;
