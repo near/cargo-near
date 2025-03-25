cc1037ea10821c2f0574fdf640caafcac1f8f0ad
dj8yf0μl <noreply@nowhere.org>
2025-03-25 19:22:02 +0200

    move out [env_keys::nep330] to [near_verify_rs]

--- cargo-near-build/src/env_keys.rs
+++ cargo-near-build/src/env_keys.rs
@@ -27,47 +27,21 @@ pub(crate) const CARGO_NEAR_ABI_PATH: &str = "CARGO_NEAR_ABI_PATH";
 pub(crate) const CARGO_NEAR_VERSION: &str = "CARGO_NEAR_VERSION";
 pub(crate) const CARGO_NEAR_ABI_SCHEMA_VERSION: &str = "CARGO_NEAR_ABI_SCHEMA_VERSION";
 
-/// module contains variables, which are set to configure build with WASM reproducibility,
-/// which correspond to some fields of `ContractSourceMetadata` in <https://github.com/near/NEPs/blob/master/neps/nep-0330.md>
-/// TODO #E8: move out this module to [near_verify_rs]
-/// TODO #E9: pub use near_verify_rs::env_keys as nep330;
-pub mod nep330 {
-    use crate::pretty_print;
-    use std::collections::HashMap;
+use std::collections::HashMap;
 
-    // ====================== NEP-330 1.2.0 - Build Details Extension ===========
-    /// NEP-330 1.2.0
-    pub const BUILD_ENVIRONMENT: &str = "NEP330_BUILD_INFO_BUILD_ENVIRONMENT";
-    /// NEP-330 1.2.0
-    pub const BUILD_COMMAND: &str = "NEP330_BUILD_INFO_BUILD_COMMAND";
-    /// NEP-330 1.2.0
-    pub const CONTRACT_PATH: &str = "NEP330_BUILD_INFO_CONTRACT_PATH";
-    /// NEP-330 1.2.0
-    pub const SOURCE_CODE_SNAPSHOT: &str = "NEP330_BUILD_INFO_SOURCE_CODE_SNAPSHOT";
-    // ====================== End section =======================================
+pub use near_verify_rs::env_keys as nep330;
+use near_verify_rs::env_keys::{BUILD_ENVIRONMENT, CONTRACT_PATH, SOURCE_CODE_SNAPSHOT};
 
-    // ====================== NEP-330 1.1.0 - Contract Metadata Extension ===========
-    /// NEP-330 1.1.0
-    pub const LINK: &str = "NEP330_LINK";
-    /// NEP-330 1.1.0
-    pub const VERSION: &str = "NEP330_VERSION";
-    // ====================== End section =======================================
-    #[cfg(feature = "docker")]
-    pub(crate) mod nonspec {
-        pub const SERVER_DISABLE_INTERACTIVE: &str = "CARGO_NEAR_SERVER_BUILD_DISABLE_INTERACTIVE";
+pub fn print_nep330_env() {
+    let mut env_map: HashMap<&str, String> = HashMap::new();
+    for key in [BUILD_ENVIRONMENT, CONTRACT_PATH, SOURCE_CODE_SNAPSHOT] {
+        let value = std::env::var(key).unwrap_or("unset".to_string());
+        env_map.insert(key, value);
     }
-
-    pub(crate) fn print_env() {
-        let mut env_map: HashMap<&str, String> = HashMap::new();
-        for key in [BUILD_ENVIRONMENT, CONTRACT_PATH, SOURCE_CODE_SNAPSHOT] {
-            let value = std::env::var(key).unwrap_or("unset".to_string());
-            env_map.insert(key, value);
-        }
-        tracing::info!(
-            target: "near_teach_me",
-            parent: &tracing::Span::none(),
-            "Variables, relevant for reproducible builds:\n{}",
-            pretty_print::indent_payload(&format!("{:#?}", env_map))
-        );
-    }
+    tracing::info!(
+        target: "near_teach_me",
+        parent: &tracing::Span::none(),
+        "Variables, relevant for reproducible builds:\n{}",
+        crate::pretty_print::indent_payload(&format!("{:#?}", env_map))
+    );
 }
