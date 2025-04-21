bba3e3be8842af67ecd2a4761dc557d48373da63
dj8yf0μl <noreply@nowhere.org>
2025-04-21 23:08:55 +0300

    fix changes of imports in tests

--- cargo-near-build/src/lib.rs
+++ cargo-near-build/src/lib.rs
@@ -90,7 +90,7 @@ pub mod docker {
 #[cfg(feature = "test_code")]
 pub use crate::types::cargo::metadata::CrateMetadata;
 #[cfg(feature = "test_code")]
-pub use crate::types::near::build::buildtime_env::CargoTargetDir;
+pub use crate::types::near::build::common_buildtime_env::CargoTargetDir;
 
 pub use bon;
 pub use camino;
