82d30a159fa8dadb27e14b1eab7d2e656c9c7140
garikbesson <garikbesson@gmail.com>
2025-05-23 13:52:43 +0100

    update README, add basic integration test

--- cargo-near/src/commands/new/new-project-template/tests/test_basics.rs
+++ cargo-near/src/commands/new/new-project-template/tests/test_basics.rs
@@ -8,5 +8,21 @@ async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>
     Ok(())
 }
 
-TEST_BASICS_ON_INCLUDE
+async fn test_basics_on(contract_wasm: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let sandbox = near_workspaces::sandbox().await?;
+    let contract = sandbox.dev_deploy(contract_wasm).await?;
 
+    let user_account = sandbox.dev_create_account().await?;
+
+    let outcome = user_account
+        .call(contract.id(), "set_greeting")
+        .args_json(json!({"greeting": "Hello World!"}))
+        .transact()
+        .await?;
+    assert!(outcome.is_success());
+
+    let user_message_outcome = contract.view("get_greeting").args_json(json!({})).await?;
+    assert_eq!(user_message_outcome.json::<String>()?, "Hello World!");
+
+    Ok(())
+}
