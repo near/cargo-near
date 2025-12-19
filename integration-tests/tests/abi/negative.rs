d35e0b4411cea0cb77a3c583cdb3e3111a2de985
Vlad Frolov <frolvlad@gmail.com>
2025-12-19 21:46:27 +0100

    fmt

--- integration-tests/tests/abi/negative.rs
+++ integration-tests/tests/abi/negative.rs
@@ -3,7 +3,9 @@ use function_name::named;
 
 #[test]
 #[named]
-#[should_panic(expected = "missing `__abi-generate` required feature for `near-sdk` dependency: probably unsupported `near-sdk` version. expected 4.1.* or higher")]
+#[should_panic(
+    expected = "missing `__abi-generate` required feature for `near-sdk` dependency: probably unsupported `near-sdk` version. expected 4.1.* or higher"
+)]
 fn test_abi_old_sdk() {
     generate_abi_fn_with! {
         Cargo: "/templates/negative/_Cargo_old_sdk.toml";
@@ -14,7 +16,9 @@ fn test_abi_old_sdk() {
 
 #[test]
 #[named]
-#[should_panic(expected = "Error invoking `cargo metadata`. Your `Cargo.toml` file is likely malformed")]
+#[should_panic(
+    expected = "Error invoking `cargo metadata`. Your `Cargo.toml` file is likely malformed"
+)]
 fn test_abi_weird_version() {
     generate_abi_fn_with! {
         Cargo: "/templates/negative/_Cargo_malformed.toml";
