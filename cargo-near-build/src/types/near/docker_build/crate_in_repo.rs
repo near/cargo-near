8bc403894515d8564047cb6bdab3c9825c498944
Vlad Frolov <frolvlad@gmail.com>
2026-01-01 20:59:22 +0100

    replace 'home' dependency with dirs-next as 'home' is an internal crate that is not designed to be used by thirdparty crates

--- cargo-near-build/src/types/near/docker_build/crate_in_repo.rs
+++ cargo-near-build/src/types/near/docker_build/crate_in_repo.rs
@@ -18,7 +18,9 @@ impl Crate {
 
         // NOTE: this cycle logic is needed to detect top level repo, when called from a
         // current dir within a submodule
-        while let Ok(repo_root) = git2::Repository::discover_path(&search_from, home::home_dir()) {
+        while let Ok(repo_root) =
+            git2::Repository::discover_path(&search_from, dirs_next::home_dir())
+        {
             repo = Some(git2::Repository::open(&repo_root)?);
             let workdir = repo
                 .as_ref()
